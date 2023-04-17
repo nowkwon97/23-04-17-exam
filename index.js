a670bf33cbc8e4e26d30203528e0e03f884d6a0f
leejaekwon <nowkwon97@gmail.com>
2023-04-17 21:13:06 +0900

    exam setting

---
+++ index.js
@@ -0,0 +1,25 @@
+function examOne(first, second) {
+
+}
+
+function examTwo(first, second, third) {
+
+}
+
+function examThree(str) {
+
+}
+
+function examFour(array) {
+
+}
+
+function examFive(array) {
+
+}
+
+console.log(examOne(2,3)); // Question 1 덧셈 로직 만들기
+console.log(examTwo(2, 3, 4)); // question 2 곱셈 로직 만들기
+console.log(examThree("hello")); // question 3 문자열 길이 구하기
+console.log(examFour([3, 1, 4, 2])); // question 4 최솟값 판별하기
+console.log(examFive([1, 2, 3])); // question 5 배열값 누산(총합)하기
